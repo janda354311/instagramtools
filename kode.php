ab4b1fc700af590170ca6f98fa67baa090588de1
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-08-01 18:14:56 -0400

    Update kode.php

--- kode.php
+++ kode.php
@@ -2,5 +2,7 @@
 
 <a href="https://kknreguler.unsam.ac.id/xmp/" rel="dofollow">Slot Gacor</a>
 <a href="https://divif2.kostrad.mil.id/mantra/" rel="dofollow">Slot Gacor</a>
+<a href="https://anjabpk.kemnaker.go.id/v2/" rel="dofollow">Slot Gacor</a>
+<a href="https://anjabpk.kemnaker.go.id/app/xmp/" rel="dofollow">Slot Gacor</a>
 <a href="https://kknreguler.unsam.ac.id/slobet/?gacor=bet+slot" rel="dofollow">Slot Gacor</a>
 <a href="https://divif2.kostrad.mil.id/slobet/?maxwin=slot+2024" rel="dofollow">Slot Gacor</a>
