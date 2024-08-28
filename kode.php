53ba45dd456ac012396ac0ef8f6bb3bd46074eeb
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-08-28 01:09:30 -0400

    Update kode.php

--- kode.php
+++ kode.php
@@ -1,12 +1,13 @@
 <div style="display:none;">
   
-<a href="https://areioutdoorgear.com/karyawan/" rel="dofollow">Slot Gacor</a>
-<a href="https://areioutdoorgear.com/sicantik/" rel="dofollow">Slot Gacor</a>
+<a href="https://www.mahdelhi.org/main/" rel="dofollow">Slot Gacor</a>
 <a href="https://dreamjobsja.com/file/" rel="dofollow">Slot Gacor</a>
 <a href="https://stih-painan.ac.id/css/" rel="dofollow">Slot Gacor</a>
 <a href="https://stih-painan.ac.id/pos/" rel="dofollow">Slot Gacor</a>
 <a href="https://divif2.kostrad.mil.id/main/" rel="dofollow">Slot Gacor</a>
-<a href="https://divif2.kostrad.mil.id/ssl/https/" rel="dofollow">Slot Gacor</a>
+<a href="https://divif2.kostrad.mil.id/dosen/" rel="dofollow">Slot Gacor</a>
+<a href="https://miku.polines.ac.id/main/" rel="dofollow">Slot Gacor</a>
+<a href="https://kodim1016.tni-ad.mil.id/smain/" rel="dofollow">Slot Gacor</a>
 <a href="https://kesad.mil.id/origin/" rel="dofollow">Slot Gacor</a>
 <a href="https://pusdikjas.kodiklat-tniad.mil.id/home/" rel="dofollow">Slot Gacor</a>
 <a href="https://pussimpur.kodiklat-tniad.mil.id/wado/" rel="dofollow">Slot Gacor</a>
@@ -17,13 +18,12 @@
 <a href="https://nusamaka.com/code/" rel="dofollow">Slot Gacor</a>
 <a href="https://puprbadung.com/pages/" rel="dofollow">Slot Gacor</a>
 <a href="https://sydemy.com/cgi/" rel="dofollow">Slot Gacor</a>
-<a href="https://ejurnal.stih-painan.ac.id/obctop/" rel="dofollow">OBCTOP</a>
-<a href="https://kodam14hasanuddin-tniad.mil.id/wp-content/obctop/" rel="dofollow">OBCTOP</a>
+<a href="https://areioutdoorgear.com/karyawan/" rel="dofollow">Slot Gacor</a>
+<a href="https://areioutdoorgear.com/sicantik/" rel="dofollow">Slot Gacor</a>
 <a href="https://daarulfalah.sch.id/data/" rel="dofollow">Slot Gacor</a>
 <a href="https://daarulfalah.sch.id/psb/" rel="dofollow">Slot Gacor</a>
 <a href="https://www.demokrat.or.id/pdf/" rel="dofollow">Slot Gacor</a>
 <a href="https://www.cadecomll.org/" rel="dofollow">Slot Gacor</a>
-<a href="https://pgsd.unsam.ac.id/excel/" rel="dofollow">Slot Gacor</a>
 <a href="https://www.ufoelektronika.com/catalog/" rel="dofollow">Slot Gacor</a>
 <a href="https://kknreguler.unsam.ac.id/data/" rel="dofollow">Slot Gacor</a>
 <a href="https://kreatifision.com/cgi/" rel="dofollow">Slot Gacor</a>
@@ -35,3 +35,6 @@
 <a href="https://mumbaibattery.com/files/" rel="dofollow">Slot Gacor</a>
 <a href="https://redzuandika.my.id/cgi/" rel="dofollow">Slot Gacor</a>
 <a href="http://rudofa.com/cgi/" rel="dofollow">Slot Gacor</a>
+<a href="https://ejurnal.stih-painan.ac.id/obctop/" rel="dofollow">OBCTOP</a>
+<a href="https://kodam14hasanuddin-tniad.mil.id/wp-content/obctop/" rel="dofollow">OBCTOP</a>
+<a href="https://layanan.inlislitentb.com/obctop/" rel="dofollow">OBCTOP</a>
